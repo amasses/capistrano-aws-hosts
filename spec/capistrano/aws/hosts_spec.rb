5f986b5e397481b3cc78a36ce395d9be9b467f29
Matthew Savage <matt@amasses.net>
2018-02-14 15:44:25 +1100

    Cleaning up

--- spec/capistrano/aws/hosts_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Capistrano::Aws::Hosts do
-  it "has a version number" do
-    expect(Capistrano::Aws::Hosts::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
